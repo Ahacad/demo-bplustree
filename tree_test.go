8aeb6e38b7192b3e1980af058014b39e5aebf6c5
ahacad <ahacad@126.com>
2021-07-01 21:24:22 +0800

    feat(tests): finish testing

--- tree_test.go
+++ tree_test.go
@@ -44,6 +44,69 @@ func verifyRoot(b *BTree, t *testing.T) {
 	}
 }
 
+func verifyNode(n node, parent *interiorNode, t *testing.T) {
+	switch nn := n.(type) {
+	case *interiorNode:
+		if nn.count < MAXKC/2 {
+			t.Errorf("interior.min.child: want >= %d, got = %d", MAXKC/2, nn.count)
+		}
+		if nn.count > MAXKC {
+			t.Errorf("interior.max.child: wnat <= %d, got = %d", MAXKC, nn.count)
+		}
+		if nn.parent() != parent {
+			t.Errorf("interior.parent: want = %p, got = %p", parent, nn.parent())
+		}
+
+		var last int
+		for i := 0; i < nn.count; i++ {
+			key := nn.kcs[i].key
+			if key != 0 && key < last {
+				t.Errorf("interior.sort.key: want > %d, got = %d", last, key)
+			}
+			last = key
+
+			if i == nn.count-1 && key != 0 {
+				t.Errorf("interior.last.key: want = 0, got = %d", key)
+			}
+			verifyNode(nn.kcs[i].child, nn, t)
+		}
+
+	case *leafNode:
+		if nn.parent() != parent {
+			t.Errorf("leaf.parent: want = %p got = %p", parent, nn.parent())
+		}
+		if nn.count < MAXKV/2 {
+			t.Errorf("leaf.min.child: want = %d, got = %d", MAXKV/2, nn.count)
+		}
+		if nn.count > MAXKV {
+			t.Errorf("leaf.max.child: want <= %d, got = %d", MAXKV, nn.count)
+		}
+	}
+}
+
+func verifyLeaf(leftMost *leafNode, count int, t *testing.T) {
+	curr := leftMost
+	last := 0
+	c := 0
+
+	for curr != nil {
+		for i := 0; i < curr.count; i++ {
+			key := curr.kvs[i].key
+
+			if key <= last {
+				t.Errorf("leaf.sort.key: want > %d, got = %d", last, key)
+			}
+			last = key
+			c++
+		}
+		curr = curr.next
+	}
+
+	if c != count {
+		t.Errorf("leaf.count: want = %d, got = %d", count, c)
+	}
+}
+
 func findLeftMost(n node) *leafNode {
 	switch nn := n.(type) {
 	case *interiorNode:
